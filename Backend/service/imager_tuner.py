a2251bbb30e134e8ca2098ccd2ad149501f00421
LAFFI01 <khatrijr01@gmail.com>
2025-12-04 21:55:51 +0545

    Little_update

--- Backend/service/imager_tuner.py
+++ Backend/service/imager_tuner.py
@@ -21,12 +21,3 @@ def tunning(path:str,type:str,size:tuple):
     
     processed_img = cv2.resize(img, size)
     return processed_img
-
-if __name__ == "__main__":
-    # This block will only run when the script is executed directly
-    # It's useful for testing the function.
-    cv2.namedWindow("output", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("output", 900, 900)
-    cv2.imshow("output", tunning(setting.File_path, "grayscale", (430, 860)))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
